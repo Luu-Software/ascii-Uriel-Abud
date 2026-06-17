ea83d1acf6f77765af4bd99ccca69ba0bd5c6899
Uriel-Abud <uriabud@gmail.com>
2026-06-17 16:07:04 -0300

    Terminé la Consigna Base

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,22 @@
 import { preguntar } from './lib/consola.ts';
 
 function repetirLetra(letra: string, cantidad: number): string {
-  // COMPLETAR
+  let letrasporlínea: string = "";
+
+  while (cantidad > 0) {
+    letrasporlínea = letrasporlínea + letra;
+    cantidad = cantidad - 1;
+  }
+
+  return letrasporlínea;
 }
 
 let caracter: string = preguntar('¿Con que caracter vas a dibujar el cuadrado? ');
 let tamaño: number = Number(preguntar('¿De qué tamaño? '));
 
-// COMPLETAR
+let resultado: string = repetirLetra (caracter, tamaño);
+
+while (tamaño > 0) {
+  console.log (resultado);
+  tamaño = tamaño - 1;
+}
