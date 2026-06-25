144293d5d38e24ee2ef71bd9a6ff1fabec6dea01
Uriel-Abud <uriabud@gmail.com>
2026-06-25 12:05:37 -0300

    Traté de avanzar un poco la Consigna manija pero no funcionó

--- src/manija.ts
+++ src/manija.ts
@@ -1,6 +1,15 @@
 import { preguntar } from './lib/consola.ts';
 
 function repetirLetra(letra: string, cantidad: number): string {
+  let letrasporlínea2: string = "";
+
+  while (cantidad > 0) {
+    letrasporlínea2 = letrasporlínea2 + letra;
+    cantidad = cantidad - 1;
+  }
+
+  return letrasporlínea2;
+  
   // COMPLETAR o importar desde index.ts
 }
 
@@ -8,10 +17,35 @@ function filaTrianguloIsoceles(letra: string, fila: number, tamaño: number): st
   // COMPLETAR
   /* fila n: 2*n+1 asteriscos, con espacios a izquierda a derecha 
   Pueden asumir que el tamaño es impar, así el triángulo queda centrado. */
+  let cantidadletras = 2 * fila + 1;
+  let cantidadespacios = tamaño - fila;
+
+  let espacios = repetirLetra ("", cantidadespacios);
+  let letras = repetirLetra (letra, cantidadletras);
+
+  return espacios + letras + espacios;
 }
 
 let caracter: string = preguntar('¿Con que caracter vas a dibujar el triángulo? ');
 let tamaño: number = Number(preguntar('¿De qué tamaño? '));
-let tipo: string = preguntar('¿Qué tipo de triángulo? (isoceles, rectangulo) ');
+let tipo: string = preguntar('¿Qué tipo de triángulo? isoceles/rectangulo: ');
 
 // COMPLETAR
+
+if (tipo === "rectangulo") {
+  while (tamaño > 0) {
+    let fila = repetirLetra (caracter, (0 + 1));
+    console.log (fila);
+    tamaño = tamaño - 1;
+  }
+}
+else if (tipo === "isoceles") {
+  while (tamaño > 0) {
+    let fila = filaTrianguloIsoceles (caracter, 0, tamaño);
+    console.log (fila);
+    tamaño = tamaño - 1;
+  }
+}
+else {
+  console.log ("Ingresá un tipo de triángulo válido.");
+}
